890be90ec84930dcc832b04acacf26482bcffe42
Rafael Salerno <salerno.rafael@gmail.com>
2016-09-15 10:45:55 -0300

    strat sandbox load balancer

---
+++ load-balancer/hello-service/src/main/java/org/gradle/HelloApplication.java
@@ -0,0 +1,38 @@
+package org.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+@RestController
+@SpringBootApplication
+public class HelloApplication {
+
+	private static Logger log = LoggerFactory.getLogger(HelloApplication.class);
+
+	@RequestMapping(value = "/greeting")
+	public String greet() {
+		log.info("Access /greeting");
+
+		List<String> greetings = Arrays.asList("Hi there", "Greetings", "Salutations");
+		Random rand = new Random();
+
+		int randomNum = rand.nextInt(greetings.size());
+		return greetings.get(randomNum);
+	}
+
+	@RequestMapping(value = "/")
+	public String home() {
+		log.info("Access /");
+		return "Hi!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+}
--- service-discovery/eureka-server-sandbox/src/main/java/org/gradle/ApplicationEurekaServer.java
+++ service-discovery/eureka-server-sandbox/src/main/java/org/gradle/ApplicationEurekaServer.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class ApplicationEurekaServer {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ApplicationEurekaServer.class).web(true).run(args);
+        new SpringApplicationBuilder(ApplicationEurekaServer.class)
+        	.web(true)
+        	.run(args);
     }
 }
