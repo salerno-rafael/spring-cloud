2611f1c4df68fb917139e80a94e81c1443923fe5
Rafael Salerno <salerno.rafael@gmail.com>
2016-09-16 10:14:34 -0300

    sandbox to load balancer

--- load-balancer/user-service/src/main/java/org/gradle/UserApplication.java
+++ load-balancer/user-service/src/main/java/org/gradle/UserApplication.java
@@ -2,6 +2,8 @@ package org.gradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,23 +13,25 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
+@RibbonClient(name = "hello-service", configuration = HelloServiceConfiguration.class)
 public class UserApplication {
 
-  @Bean
-  RestTemplate restTemplate(){
-    return new RestTemplate();
-  }
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
-  @Autowired
-  RestTemplate restTemplate;
+	@Autowired
+	RestTemplate restTemplate;
 
-  @RequestMapping("/hi")
-  public String hi(@RequestParam(value="name", defaultValue="Artaban") String name) {
-    String greeting = this.restTemplate.getForObject("http://localhost:8090/greeting", String.class);
-    return String.format("%s, %s!", greeting, name);
-  }
+	@RequestMapping("/hi")
+	public String hi(@RequestParam(value = "name", defaultValue = "Artaban") String name) {
+		String greeting = this.restTemplate.getForObject("http://hello-service/greeting", String.class);
+		return String.format("%s, %s!", greeting, name);
+	}
 
-  public static void main(String[] args) {
-    SpringApplication.run(UserApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
 }
