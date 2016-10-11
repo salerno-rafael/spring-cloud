37618c6265cc55dd37fa6d32838b534fed74681d
Rafael Salerno <rafaelsalerno@Rafaels-MacBook-Pro.local>
2016-10-11 09:27:09 -0300

    new sandbox

--- configuration/archaius-sandbox/src/main/java/org/gradle/ArchaiusSandBox.java
+++ configuration/archaius-sandbox/src/main/java/org/gradle/ArchaiusSandBox.java
@@ -2,41 +2,44 @@ package org.gradle;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
 import com.netflix.config.AbstractPollingScheduler;
 import com.netflix.config.ConfigurationManager;
 import com.netflix.config.DynamicConfiguration;
 import com.netflix.config.DynamicPropertyFactory;
-import com.netflix.config.DynamicStringProperty;
 import com.netflix.config.FixedDelayPollingScheduler;
 import com.netflix.config.PolledConfigurationSource;
 import com.netflix.config.sources.URLConfigurationSource;
 
-public class ArchaiusTest {
-
+public class ArchaiusSandBox {
+	
+	
 	static {
 		System.setProperty("archaius.configurationSource.defaultFileName", "archaius.properties");
 	}
 
 	public String configuration() throws IOException {
 		
+		installConfig();
+		ConfigurationManager.getDeploymentContext().setDeploymentEnvironment(System.getenv("environment"));
+		ConfigurationManager.loadCascadedPropertiesFromResources("archaius");
+		return DynamicPropertyFactory.getInstance().getStringProperty("myprop", "NOT FOUND").get();
+	}
+
+	private void installConfig() {
 		if (!ConfigurationManager.isConfigurationInstalled()) {
 			AbstractPollingScheduler scheduler = new FixedDelayPollingScheduler(100, 1000, true);
 			PolledConfigurationSource source = new URLConfigurationSource();
 			DynamicConfiguration dynamicConfig = new DynamicConfiguration(source, scheduler);
 			ConfigurationManager.install(dynamicConfig);
 		}
-		ConfigurationManager.getDeploymentContext().setDeploymentEnvironment(System.getenv("environment"));
-		ConfigurationManager.loadCascadedPropertiesFromResources("archaius");
-		DynamicStringProperty myprop = DynamicPropertyFactory.getInstance().getStringProperty("myprop", "NOT FOUND");
-		return myprop.get();
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		System.out.println(System.getenv("environment"));
-		ArchaiusTest a = new ArchaiusTest();
-		System.out.println(a.configuration());
+		System.out.println("Currrent Environment Variable = "+ System.getenv("environment"));
+		ArchaiusSandBox arch = new ArchaiusSandBox();
+		System.out.println(arch.configuration());
 		TimeUnit.SECONDS.sleep(10);
-		System.out.println(a.configuration());
-		System.out.println(new ArchaiusTest().configuration());
+		System.out.println(arch.configuration());
 	}
 }
