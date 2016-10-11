42a29816f6156706b0e9f02a8e90d8684bdaed44
Rafael Salerno <rafaelsalerno@Rafaels-MacBook-Pro.local>
2016-10-11 09:20:16 -0300

    new sandbox

--- configuration/archaius-sandbox/src/main/java/org/gradle/ArchaiusTest.java
+++ configuration/archaius-sandbox/src/main/java/org/gradle/ArchaiusTest.java
@@ -2,11 +2,7 @@ package org.gradle;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.configuration.AbstractConfiguration;
-
 import com.netflix.config.AbstractPollingScheduler;
-import com.netflix.config.ConcurrentCompositeConfiguration;
 import com.netflix.config.ConfigurationManager;
 import com.netflix.config.DynamicConfiguration;
 import com.netflix.config.DynamicPropertyFactory;
@@ -16,34 +12,31 @@ import com.netflix.config.PolledConfigurationSource;
 import com.netflix.config.sources.URLConfigurationSource;
 
 public class ArchaiusTest {
-	
-	 static {
-		    System.setProperty("archaius.configurationSource.defaultFileName", "archaius.properties");
-		  }
-	 
-	 public String propertie ; 
-	 
-	public void test() throws IOException {
-	
-		AbstractPollingScheduler scheduler = new FixedDelayPollingScheduler(100, 1000, 	true);
-		PolledConfigurationSource source = new URLConfigurationSource(); 
-		DynamicConfiguration dynamicConfig = new DynamicConfiguration(source, scheduler);
-	     ConfigurationManager.install(dynamicConfig);
-	     
-		ConfigurationManager.loadCascadedPropertiesFromResources("archaius");
-		DynamicStringProperty myprop = DynamicPropertyFactory.getInstance().getStringProperty("myprop","NOT FOUND");
-		
-		System.out.println(myprop.get());
-		propertie = myprop.get();
 
+	static {
+		System.setProperty("archaius.configurationSource.defaultFileName", "archaius.properties");
 	}
-	
+
+	public String configuration() throws IOException {
+		
+		if (!ConfigurationManager.isConfigurationInstalled()) {
+			AbstractPollingScheduler scheduler = new FixedDelayPollingScheduler(100, 1000, true);
+			PolledConfigurationSource source = new URLConfigurationSource();
+			DynamicConfiguration dynamicConfig = new DynamicConfiguration(source, scheduler);
+			ConfigurationManager.install(dynamicConfig);
+		}
+		ConfigurationManager.getDeploymentContext().setDeploymentEnvironment(System.getenv("environment"));
+		ConfigurationManager.loadCascadedPropertiesFromResources("archaius");
+		DynamicStringProperty myprop = DynamicPropertyFactory.getInstance().getStringProperty("myprop", "NOT FOUND");
+		return myprop.get();
+	}
+
 	public static void main(String[] args) throws IOException, InterruptedException {
-		System.out.println(System.getenv("env"));
-		ArchaiusTest a=	new ArchaiusTest();
-		a.test();
+		System.out.println(System.getenv("environment"));
+		ArchaiusTest a = new ArchaiusTest();
+		System.out.println(a.configuration());
 		TimeUnit.SECONDS.sleep(10);
-		System.out.println("dasds333333adasdsad");
-		System.out.println(a.propertie);
+		System.out.println(a.configuration());
+		System.out.println(new ArchaiusTest().configuration());
 	}
 }
